7d87d04bfec531d495e5bea436bb9607a6c51967
Sky_miner <458482582@qq.com>
2020-07-14 14:48:29 +0800

    add two empty activity

--- app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,40 @@
 package com.bytedance.videoplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1;
+    private Button btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ImageActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,VideoActivity.class));
+            }
+        });
     }
 }
